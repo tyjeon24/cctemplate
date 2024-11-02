acefcfdd72640fc2f5161d20a2e755b67ff0b321
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-11-02 16:30:27 +0900

    u

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+import toml
+
+toml_file = "pyproject.toml"
+
+with open(toml_file, "r") as f:
+    project = toml.load(f)
+
+if "dependencies" not in project.get("project", {}):
+    project.setdefault("project", {})["dependencies"] = []
+
+dependencies = project["project"]["dependencies"]
+if not any(dep.startswith("pandas") for dep in dependencies):
+    dependencies.append("pandas")
+
+with open(toml_file, "w") as f:
+    toml.dump(project, f)
