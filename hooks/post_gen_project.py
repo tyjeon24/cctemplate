58a6abe716ea80c7de5856f8e7da81a8db5b559a
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-11-02 16:57:05 +0900

    a

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,11 @@
-import toml
 
-toml_file = "../pyproject.toml"
+import toml
+from pathlib import Path
+
+
+toml_file = Path("../pyproject.toml")
+temp_toml_file = Path("{{cookiecutter.project_name}}/pyproject.toml")
+project_name_dir = Path("{{cookiecutter.project_name}}")
 
 with open(toml_file, "r") as f:
     project = toml.load(f)
@@ -14,3 +19,6 @@ if not any(dep.startswith("pandas") for dep in dependencies):
 
 with open(toml_file, "w") as f:
     toml.dump(project, f)
+    
+temp_toml_file.unlink()
+project_name_dir.rmdir()
