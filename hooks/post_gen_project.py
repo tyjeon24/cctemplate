4538c0d0e139a1d2aa8922d231ff7f435c1a03b3
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-11-02 17:39:16 +0900

    u

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,15 +10,11 @@ project_name_dir = Path("../{{cookiecutter.project_name}}")
 with open(toml_file, "r") as f:
     project = toml.load(f)
 
-if "dependencies" not in project.get("project", {}):
-    project.setdefault("project", {})["dependencies"] = []
-
-dependencies = project["project"]["dependencies"]
-if not any(dep.startswith("pandas") for dep in dependencies):
-    dependencies.append("pandas")
+with open(temp_toml_file, "r") as f:
+    template = toml.load(f)
 
 with open(toml_file, "w") as f:
-    toml.dump(project, f)
+    toml.dump(project.update(template), f)
 
 # Cleanup
 temp_toml_file.unlink(missing_ok=True)
